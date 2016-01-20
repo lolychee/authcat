e3a989978558c06334bf6bb95ff403f34cb03ba7
lychee <lolychee@gmail.com>
2016-01-20 23:53:10 +0800

    添加dummy app

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,9 @@ ENV["RAILS_ENV"] = "test"
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'authcat'
 
+require File.expand_path("../dummy/config/environment", __FILE__)
+ActiveRecord::Migrator.migrations_paths = [File.expand_path("../dummy/db/migrate", __FILE__)]
+
 Dir[File.expand_path("../support/**/*.rb", __FILE__)].each { |f| require f }
 
 RSpec.configure do |config|
