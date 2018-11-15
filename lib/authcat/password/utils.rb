9344cd457ba6fd1cd82f5483211868ca847b410b
lychee xing <lolychee@gmail.com>
2018-11-15 16:56:52 +0800

    feat: 添加简单密码随机生成方法

--- lib/authcat/password/utils.rb
+++ lib/authcat/password/utils.rb
@@ -9,6 +9,10 @@ module Authcat
         a.bytesize == b.bytesize &&
         a.each_byte.zip(b.each_byte).map { |a, b| a ^ b }.reduce(:|).zero?
       end
+
+      def random_password(length = 32)
+        SecureRandom.hex(length / 2)
+      end
     end
   end
 end
