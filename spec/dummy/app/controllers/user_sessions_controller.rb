9601fc12b82f4caf7f36e3b54b90e11098d1da28
lychee xing <lolychee@gmail.com>
2018-11-17 23:59:50 +0800

    feat: 登录增加两步验证

--- spec/dummy/app/controllers/user_sessions_controller.rb
+++ spec/dummy/app/controllers/user_sessions_controller.rb
@@ -28,6 +28,6 @@ class UserSessionsController < ApplicationController
   private
 
     def user_session_params
-      params.require(:user_session).permit(:identifier, :password, :remember_me)
+      params.require(:user_session).permit(:identifier, :password, :remember_me, :token, :otp_code)
     end
 end
--- spec/dummy/app/forms/user_session.rb
+++ spec/dummy/app/forms/user_session.rb
@@ -4,29 +4,52 @@ class UserSession
   include ActiveModel::Model
   include ActiveModel::Attributes
 
-  include Authcat::Password::Validators
+  include Authcat::Model
 
   attribute :identifier,  :string
   attribute :password,    :string
   attribute :remember_me, :boolean
+  attribute :token,       :string
+  attribute :otp_code,    :string
 
   attr_accessor :user
-  delegate :password_digest, to: :user, allow_nil: true
+  delegate :password_verify, :two_factor_authentication_required?, to: :user, allow_nil: true
 
-  with_options unless: :user do
-    validates :identifier, presence: true
-    validate :identifier_should_founded
+  with_options if: :token do
+    validate :token_should_founded
+    validate :otp_code_should_match
   end
 
-  validates :password, presence: true, password_verify: true
+  with_options unless: :token do
+    validates :identifier, presence: true
+    validate :identifier_should_founded
+    validates :password, presence: true, password_verify: true
+    validate :valid_two_factor_authentication_required?
+  end
 
   def save(*)
     valid?
+  end
+
+  def generate_token
+    User.tokenize(user, expires_in: 5.minutes, secret_key: "user_session")
   end
 
   private
 
     def identifier_should_founded
       errors.add(:identifier, "not found") unless self.user = User.find_by_identifier(identifier)
+    end
+
+    def token_should_founded
+      errors.add(:token, "not found") unless self.user = User.untokenize(token, secret_key: "user_session") rescue nil
+    end
+
+    def valid_two_factor_authentication_required?
+      errors.add(:base, "two-factor authentication required") if two_factor_authentication_required?
+    end
+
+    def otp_code_should_match
+      errors.add(:otp_code, "not match") unless self.user.otp_verify(otp_code)
     end
 end
