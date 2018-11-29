680458822b7aec944561a50a196783d7b8640f19
lychee xing <lolychee@gmail.com>
2018-11-29 23:35:46 +0800

    refactor: 移除Authcat::Model并重构Authcat::Identity

--- lib/authcat.rb
+++ lib/authcat.rb
@@ -6,7 +6,6 @@ require "authcat/authenticator"
 require "authcat/token"
 require "authcat/password"
 require "authcat/multi_factor"
-require "authcat/model"
 require "authcat/identity"
 require "authcat/railtie"
 
--- lib/authcat/identity.rb
+++ lib/authcat/identity.rb
@@ -5,17 +5,24 @@ module Authcat
     extend Supports::Registrable
 
     def self.included(base)
-      base.include Authcat::Model
+      base.extend ClassMethods
 
+      base.include Token::Tokenable
       base.tokenable
-      base.include Password
-      base.include TwoFactorAuthentication
+    end
+
+    module ClassMethods
+      def authcat(mod_name)
+        self.include Identity.lookup(mod_name)
+      end
     end
 
     module Password
       def self.included(base)
-        base.has_secure_password
+        base.include Authcat::Password::SecurePassword,
+                     Authcat::Password::Validators
 
+        base.has_secure_password :password
         base.validates :password, presence: true, on: :create
       end
 
@@ -24,19 +31,15 @@ module Authcat
       end
     end
 
-    module TwoFactorAuthentication
+    module TwoFactorAuth
       def self.included(base)
-        base.has_one_time_password :otp
-        base.has_backup_codes :otp_backup_codes
-      end
-
-      def two_factor_authentication_required?
-        !!otp
-      end
-
-      def authenticate(password, allow_backup_code: false)
-        super(password) && (allow_backup_code && otp_backup_codes_verify(password, revoke: true) && self)
+        base.include MultiFactor::TwoFactorAuth
+        base.has_two_factor_auth :otp
       end
     end
+
+    register :password, Password
+    register :two_factor_auth, TwoFactorAuth
+
   end
 end
