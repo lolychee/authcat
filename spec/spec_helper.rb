63ac9d96115bd8033291e2e4330c3b83e4342301
lychee xing <lolychee@gmail.com>
2018-12-27 20:54:15 +0800

    refactor: 优化authcat.gemspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,6 +28,16 @@ RSpec.configure do |config|
     end
   end
 
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
   # These two settings work together to allow you to limit a spec run
   # to individual examples or groups you care about by tagging them with
   # `:focus` metadata. When nothing is tagged with `:focus`, all examples
