fcbdb9c41a905c72184db626c5d86d6cffd92f92
lychee xing <lolychee@gmail.com>
2018-12-28 13:56:45 +0800

    fix: ActiveModel不存在时不定义FoundValidator

--- lib/authcat/password/validators/found_validator.rb
+++ lib/authcat/password/validators/found_validator.rb
@@ -3,30 +3,34 @@
 module Authcat
   module Password
     module Validators
-      class FoundValidator < ActiveModel::EachValidator
-        def validate_each(record, attribute, value)
-          klass = record.class
-          find_method_name = "find_by_#{attribute}"
+      if defined?(ActiveModel) && defined?(ActiveModel::EachValidator)
 
-          found = if klass.respond_to?(find_method_name)
-            klass.send(find_method_name, value)
-          else
-            klass.find_by(attribute => value)
+        class FoundValidator < ActiveModel::EachValidator
+          def validate_each(record, attribute, value)
+            klass = record.class
+            find_method_name = "find_by_#{attribute}"
+
+            found = if klass.respond_to?(find_method_name)
+              klass.send(find_method_name, value)
+            else
+              klass.find_by(attribute => value)
+            end
+
+            if found
+              changes = record.changed_attributes
+              coder = {}
+              found.encode_with(coder)
+              record.init_with(coder)
+              record.attributes = changes
+
+              # FIX: record.attribute_was bug; ActiveModel::Dirty related
+              record.instance_variable_set(:@mutations_from_database, nil)
+            end
+
+            record.errors[attribute] << (options[:message] || "is not found") unless found
           end
-
-          if found
-            changes = record.changed_attributes
-            coder = {}
-            found.encode_with(coder)
-            record.init_with(coder)
-            record.attributes = changes
-
-            # FIX: record.attribute_was bug; ActiveModel::Dirty related
-            record.instance_variable_set(:@mutations_from_database, nil)
-          end
-
-          record.errors[attribute] << (options[:message] || "is not found") unless found
         end
+
       end
     end
   end
