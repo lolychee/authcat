07d77c0537682b1c6408ac48e47bb39cb127cc27
lychee xing <lolychee@gmail.com>
2019-03-10 15:40:25 +0800

    refactor: supports/registrable.rb合并到supports.rb

--- lib/authcat/supports.rb
+++ lib/authcat/supports.rb
@@ -1,3 +1,19 @@
 # frozen_string_literal: true
 
-require "authcat/supports/registrable"
+module Authcat
+  module Supports
+    module Registrable
+      def registry
+        @registry ||= Hash.new { |_, name| raise NameError, "Unknown #{name.inspect}" }
+      end
+
+      def register(name, value)
+        registry[name] = value
+      end
+
+      def lookup(name)
+        registry[name]
+      end
+    end
+  end
+end
