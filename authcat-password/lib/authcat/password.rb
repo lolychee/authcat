25b56131b23aa9943f9c79fb8dc00b238668d452
lychee xing <lolychee@gmail.com>
2020-10-08 13:23:31 +0000

    init authcat-password

---
+++ authcat-password/lib/authcat/password.rb
@@ -0,0 +1,8 @@
+require "authcat/password/version"
+
+module Authcat
+  module Password
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ authcat-password/spec/authcat/password_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Authcat::Password do
+  it "has a version number" do
+    expect(Authcat::Password::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
