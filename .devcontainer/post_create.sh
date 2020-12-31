a1e949f9741ed8b7b36f146d28eacb5037d37ce5
lychee xing <lolychee@gmail.com>
2020-12-31 07:33:48 +0000

    chore: 更新开发配置

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 
 gem sources --add https://gems.ruby-china.com/ --remove https://rubygems.org/
-gem install bundler solargraph
+gem install bundler solargraph steep
 solargraph download-core $RUBY_VERSION
 
-bundle config mirror.https://rubygems.org/ https://gems.ruby-china.com/
-bundle install
+bundle config set mirror.https://rubygems.org/ https://gems.ruby-china.com/
+
+bin/setup
--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ source 'https://rubygems.org'
 gemspec
 
 gem 'rake', '~> 13.0'
+
 gem 'rspec', '~> 3.0'
 
 gem 'rubocop'
@@ -13,4 +14,3 @@ gem 'rubocop-performance'
 gem 'rubocop-rake'
 gem 'rubocop-rspec'
 gem 'rubocop-rubycw'
-gem 'standard'
