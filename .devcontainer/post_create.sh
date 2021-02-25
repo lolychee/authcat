5f698480b55da355942f3893a166f20006cb5a0c
lychee xing <lolychee@gmail.com>
2021-02-25 07:10:11 +0000

    chore: improve disk performance

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -6,4 +6,6 @@ solargraph download-core $RUBY_VERSION
 
 bundle config set mirror.https://rubygems.org/ https://gems.ruby-china.com/
 
+sudo chown vscode:vscode playground/tmp/cache playground/node_modules
+
 bin/setup
