b7168504ba8fb100636bbe379e0541f9ab6dd906
lychee xing <lolychee@gmail.com>
2021-02-25 07:41:41 +0000

    refactor: User添加name字段

--- playground/app/controllers/users_controller.rb
+++ playground/app/controllers/users_controller.rb
@@ -64,6 +64,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.fetch(:user, {})
+      params.require(:user).permit(:name, :email, :password)
     end
 end
--- playground/db/migrate/20210225073500_create_users.rb
+++ playground/db/migrate/20210225073500_create_users.rb
@@ -4,12 +4,10 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       # t.string :email, index: { unique: true }
       t.string :email_ciphertext
       t.string :email_bidx, index: { unique: true }
-      t.string :email_otp_secret
 
       # t.string :phone_number, index: { unique: true }
       t.string :phone_number_ciphertext
       t.string :phone_number_bidx, index: { unique: true }
-      t.string :phone_number_otp_secret
 
       t.string :password_digest
       t.string :password_otp_secret
