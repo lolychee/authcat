1980ec07a4345320547ec74fc1cc8e2fac9f1e6b
lychee xing <lolychee@gmail.com>
2022-10-14 09:54:30 +0000

    add: init Authcat::Webauthn

---
+++ authcat-webauthn/lib/authcat/webauthn/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Authcat
+  module Webauthn
+    VERSION = "0.1.0"
+  end
+end
---
+++ authcat-webauthn/spec/authcat/webauthn_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Authcat::Webauthn do
+  it "has a version number" do
+    expect(Authcat::Webauthn::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
