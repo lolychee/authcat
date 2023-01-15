5857306fb1787c4a6163659b09ca1b2b0bb1d20b
lychee xing <lolychee@gmail.com>
2023-01-15 17:36:30 +0000

    chore: update rails to 6.1.7

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -3,13 +3,12 @@
 export BUNDLE_JOBS=8
 
 # gem sources --remove https://gems.ruby-china.com/ --add https://rubygems.org/
-# gem install bundler solargraph steep
+gem install bundler # solargraph steep
 # solargraph download-core $RUBY_VERSION
 
 # bundle config set mirror.https://rubygems.org/ https://gems.ruby-china.com/
 
-sudo chown vscode:vscode -R .
-
+# sudo chown vscode:vscode -R .
 bin/setup
 cd playground
 bin/setup
