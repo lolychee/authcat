6ec61a43bded8277836cb21d4f37056ce2f8584e
lychee xing <lolychee@gmail.com>
2023-05-19 10:52:32 +0000

    feat(authcat-credential): add initial files and configuration for authcat-credential gem, including Gemfile, Rakefile, .rspec, .rubocop.yml, LICENSE.txt, README.md, CODE_OF_CONDUCT.md, and CHANGELOG.md. Also, add authcat-credential to authcat.code-workspace.
    feat(authcat-credential): add console, setup, lib, sig, and spec files for Authcat::Credential gem to implement basic functionality and testing

---
+++ authcat-credential/lib/authcat/credential.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "credential/version"
+
+module Authcat
+  module Credential
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ authcat-credential/spec/authcat/credential_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Authcat::Credential do
+  it "has a version number" do
+    expect(Authcat::Credential::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
