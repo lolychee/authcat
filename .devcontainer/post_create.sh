89c522cbbb477f5cf8e95cadbfcc92964dcb0eb3
lychee xing <lolychee@gmail.com>
2024-07-14 12:28:43 +0000

    chore: Update Ruby version to 3.2.4

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env sh
 
-# gem sources --remove https://gems.ruby-china.com/ --add https://rubygems.org/
-# gem install bundler # solargraph steep
-# solargraph download-core $RUBY_VERSION
-
 rvm fix-permissions system
-
-# bundle config set mirror.https://rubygems.org/ https://gems.ruby-china.com/
 
 # sudo chown vscode:vscode -R .
 bin/setup
